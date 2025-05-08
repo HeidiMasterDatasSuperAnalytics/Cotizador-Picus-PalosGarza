3a7eb46c539052dd05556ed62860de1a4705e4d9
HeidiMasterDatasSuperAnalytics <heidi.31032003@gmail.com>
2025-05-08 18:31:02 -0500

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.markdown("""
+# Bienvenido al Cotizador de Rutas
+
+Esta herramienta te permitirá capturar, gestionar y simular rutas de forma eficiente y precisa.  
+Utiliza el menú lateral para comenzar con:
+
+- 🛣️ **Captura de Ruta**
+- 🔁 **Simulador Vuelta Redonda**
+- 🗂️ **Gestión de Rutas**
+- 📂 **Archivos**
+
+---
+""")
